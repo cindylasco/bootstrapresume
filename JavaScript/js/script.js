deec7f493738bf4b54c3ad02f95c945c55b7af81
Ryann Pilapil <ryannpilapil@gmail.com>
2016-07-23 12:34:07 +0800

    First JavaScript Activity

---
+++ JavaScript/js/script.js
@@ -0,0 +1,8 @@
+
+	document.getElementById('title_heading').innerHTML = "My JavaScript Title Heading";
+	document.getElementById('subtitle').innerHTML= "My JavaScript Subtitle";
+	document.getElementById('tablemk').innerHTML= "<tr><th>Name</th><th>Gender</th></tr>" +
+													"<tr><td>Reymar</td><td>Male</td></tr>" +
+														"<tr><td>Cindy</td><td>Female</td></tr>" +
+															"<tr><td>Kevin</td><td>Male</td></tr>";
+
